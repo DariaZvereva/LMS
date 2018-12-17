107a271ed0573a742921c8189649a861e0d4439e
bobrovskayaa <nataliya.bobrovskaya@phystech.edu>
2018-12-17 23:22:02 +0300

    add route preliminary_registration

--- lms/app.py
+++ lms/app.py
@@ -13,18 +13,26 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 
-from utils import blank_resp, RegistrationForm, LoginForm, CourseForm
+from utils import blank_resp, PreliminaryRegistrationForm, LoginForm, CourseForm
 from Domain.Users import User
 from Domain.Courses import Course
 from Domain.Students import Student, Group
 from Domain.Teachers import Teacher
 
 def register_user_in(form):
-    user = User(username=form.username.data,
-             email=form.email.data,
-             name=form.name.data,
-             surname=form.surname.data,
-             second_name=form.second_name.data)
+    #TODO: упростить
+    user = User(
+        username=form.username.data,
+        email=form.email.data,
+        name=form.name.data,
+        surname=form.surname.data,
+        second_name=form.second_name.data,
+        group=form.group.data,
+        year=form.year.data,
+        degree=form.degree.data,
+        education=form.education.data,
+        basis=form.basis.data
+    )
     user.set_password(form.password.data)
     db.session.add(user)
     db.session.commit()
@@ -39,10 +47,14 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/register', methods=['POST'])
-def register_user():
+@app.route('/preliminary_register', methods=['POST'])
+# @login_required
+def preliminary_register_user():
+    """Администратор может добавить предопределенного пользователя (студента или преподавателя)
+
+    """
     answer = blank_resp()
-    form = RegistrationForm(request.form)
+    form = PreliminaryRegistrationForm(request.form)
     if form.validate():
         register_user_in(form)
     else:
--- lms/utils.py
+++ lms/utils.py
@@ -1,4 +1,5 @@
-from wtforms import Form, StringField, PasswordField, validators
+from wtforms import Form, StringField, PasswordField, validators,\
+    IntegerField, SelectField, RadioField
 from wtforms.validators import DataRequired, ValidationError
 from Domain.Users import User
 
@@ -15,15 +16,23 @@ class CourseForm(Form):
     description = StringField('Description', [validators.Length(min=0, max=50)])
 
 
-class RegistrationForm(Form):
+class PreliminaryRegistrationForm(Form):
     username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    email = StringField('Email Address', [validators.Email(), validators.Length(min=6, max=35)])
     name = StringField('Name', [validators.Length(min=3, max=35)])
     surname = StringField('Surname', [validators.Length(min=3, max=35)])
     second_name = StringField('Surname', [validators.Length(min=3, max=35)])
     password = PasswordField('New Password', [
         validators.DataRequired()
     ])
+    group = StringField('Group', [validators.Length(min=1, max=5)])
+    year = IntegerField('Year', [validators.NumberRange(min=1900, max=2016)])
+    degree = SelectField('Degree', choices=[
+        ('bachelor', 'bachelor'), ('specialist', 'specialist'), ('master', 'master')])
+    education = SelectField('Education', choices=[
+        ('full', 'full'), ('part', 'part'), ('evening', 'evening')])
+    basis = RadioField('Education', choices=[
+        ('contract', 'contract'), ('budget', 'budget')])
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
