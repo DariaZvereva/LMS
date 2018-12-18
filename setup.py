b633d600673265745fdb8c1992fa701a7fd05e72
DariaZvereva <zverevads@gmail.com>
2018-12-18 20:59:19 +0300

    entry point in setup.py

--- setup.py
+++ setup.py
@@ -38,5 +38,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-    ]
+    ],
+    entry_points={
+        'console_scripts': ['lms=lms.app:main'],
+    }
 )
