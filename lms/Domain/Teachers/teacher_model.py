9b821c3f33def217bb469d15345973c918e651f1
bobrovskayaa <nataliya.bobrovskaya@phystech.edu>
2018-12-19 13:50:01 +0300

    delete __tablename__ = None from Teachers

--- lms/Domain/Teachers/teacher_model.py
+++ lms/Domain/Teachers/teacher_model.py
@@ -2,6 +2,5 @@ from lms.app import db
 
 
 class Teacher(db.Model):
-    __tablename__ = None
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
