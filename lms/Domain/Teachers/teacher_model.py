97161348dbaade1453ef1d4295d6696535570f00
DariaZvereva <zverevads@gmail.com>
2018-12-19 19:09:12 +0300

    fix foreign keys for tables

--- lms/Domain/Teachers/teacher_model.py
+++ lms/Domain/Teachers/teacher_model.py
@@ -5,4 +5,4 @@ class Teacher(db.Model):
     __tablename__ = "teachers"
     __table_args__ = {"useexisting": True}
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
